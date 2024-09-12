46b7a51f9e835e795c86e012441ad4b701373b33
nieyuyao <nieyuyao0826@hotmail.com>
2024-09-12 19:48:26 +0800

    feat: calc light bolt color

--- lightning.js
+++ lightning.js
@@ -1,4 +1,4 @@
-import { Color, Object3D, Vector2 } from 'three'
+import { Color, Object3D, Vector2, Vector3 } from 'three'
 import { Line2 } from 'three/examples/jsm/lines/Line2'
 import { LineGeometry } from 'three/examples/jsm/lines/LineGeometry'
 import { LineMaterial } from 'three/examples/jsm/lines/LineMaterial'
@@ -27,17 +27,14 @@ const clamp = (min, max) => {
   return min + (max - min) * Math.random()
 }
 
-// calculate vertex color
-const calcVertexColor = (v) => {
-  // TODO:
-}
-
 export class Lightning extends Object3D {
   bolts = []
 
   generations = 5
 
   segments = []
+
+  ideallyLength = 1
 
   /**
    * @param {Vector2} start
@@ -49,42 +46,41 @@ export class Lightning extends Object3D {
     super()
     this.start = new Vector2(start.x, start.y)
     this.end = new Vector2(end.x, end.y)
+    this.ideallyLength = this.start.distanceTo(this.end)
     this.generations = generations
     this.amplitude = amplitude
-    this.bolts = [new Bolt(this.start, this.end)]
+    this.bolts = [new Bolt(this.start, this.end, 4, 1, false)]
     this.createLightBolts()
     this.createSegments()
   }
 
   createLightBolts() {
     let offsetAmount = this.amplitude
-    const startEndLength = this.start.distanceTo(this.end)
+
     for (let i = 0; i < this.generations; i++) {
       const newBolts = []
       const boltsAmount = this.bolts.length
-      const mainWidth = 2
       for (let j = 0; j < boltsAmount; j++) {
+        const parentBolt = this.bolts[j]
         const { start, end } = this.bolts[j]
         const dir = end.clone().sub(start)
         const nor = new Vector2(-dir.y, dir.x).normalize()
         const offset = nor.multiplyScalar(clamp(-offsetAmount, offsetAmount))
         const midPoint = start.clone().add(dir.clone().multiplyScalar(0.5)).add(offset)
-        newBolts.push(new Bolt(start, midPoint, mainWidth, 1, false))
+        newBolts.push(new Bolt(start, midPoint, parentBolt.width, 1, parentBolt.isBranch))
         // generate branch
-        const distance = midPoint.distanceTo(this.end)
-        const probability = distance / startEndLength
-        if (Math.random() < (probability * (boltsAmount - j)) / boltsAmount) {
+        const distanceToEnd = midPoint.distanceTo(this.end)
+        const distanceToStart = midPoint.distanceTo(this.start)
+        const probability = distanceToEnd / this.ideallyLength
+        if (distanceToStart > 40 && Math.random() < (probability * (boltsAmount - j)) / boltsAmount) {
           const branch = midPoint.clone().sub(start)
           const crossed = branch.cross(end.clone().sub(midPoint)) >= 0 ? -1 : 1
           const angle = Math.random() * probability * crossed
           const branchEndPoint = branch.multiplyScalar(probability * 2.4).add(midPoint)
           branchEndPoint.rotateAround(midPoint, angle)
-          const widthScale = 1 - Math.pow(i / this.generations, 4)
-          // set minimum opacity to 0.4
-          const opacity = Math.max(0.4, 1 - i / this.generations)
-          newBolts.push(new Bolt(midPoint, branchEndPoint, mainWidth * widthScale, opacity, true))
+          newBolts.push(new Bolt(midPoint, branchEndPoint, parentBolt.width * 0.6, parentBolt.opacity * 0.8, true))
         }
-        newBolts.push(new Bolt(midPoint, end, mainWidth, 1, false))
+        newBolts.push(new Bolt(midPoint, end, parentBolt.width, 1, parentBolt.isBranch))
       }
       offsetAmount /= 2
       this.bolts = newBolts
@@ -92,18 +88,27 @@ export class Lightning extends Object3D {
   }
 
   createSegments() {
+    const { start: lightingStart, ideallyLength } = this
     this.bolts.forEach((bolt) => {
-      const points = []
-      points.push(bolt.start.x, bolt.start.y, 0)
-      points.push(bolt.end.x, bolt.end.y, 0)
+      if (bolt.width < 0.3) {
+        return
+      }
       const geo = new LineGeometry()
       const material = new LineMaterial({
         linewidth: bolt.width,
         opacity: bolt.opacity,
         transparent: true,
-        color: new Color(1, 1, 1)
+        vertexColors: true
       })
-      geo.setPositions(points)
+      geo.setPositions([bolt.start.x, bolt.start.y, 0, bolt.end.x, bolt.end.y, 0])
+      // calculate vertex color
+      // rgb(1, 1, 1) => rgb(0.26, 0.3, 0.82)
+      const scalar = bolt.isBranch
+        ? Math.pow((bolt.start.distanceTo(lightingStart) / ideallyLength) * (2 / bolt.opacity), 2)
+        : Math.pow((bolt.start.distanceTo(lightingStart) / ideallyLength), 2)
+      const c = new Vector3(0.1, 0.1, 0.93).sub(new Vector3(1, 1, 1)).multiplyScalar(Math.min(1, scalar)).add(new Vector3(1, 1, 1))
+
+      geo.setColors([...c.toArray(), ...c.toArray()])
       const segment = new Line2(geo, material)
       this.add(segment)
       this.segments.push(segment)
